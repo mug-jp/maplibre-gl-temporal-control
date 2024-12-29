66eeff91d1d7dde731757885789c54a76b56e90e
Kanahiro <mediterranean1769@gmail.com>
2024-12-29 15:30:16 +0900

    migarte to maplibre-gl@v5

--- example/raster.ts
+++ example/raster.ts
@@ -1,107 +1,128 @@
-import maplibregl from 'maplibre-gl';
+import maplibregl, {
+	RasterLayerSpecification,
+	RasterSourceSpecification,
+	StyleSpecification,
+} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 import TemporalControl from '../src';
 
 import { nowcast } from 'jma-utils';
 
-const makeMapStyle = (xyzUrls) => {
-    const xyzSourcesLayers = xyzUrls.map((xyzUrl) =>
-        makeXyzTileSourceLayer(xyzUrl),
-    );
-    const xyzSources = xyzSourcesLayers.reduce((prev, sourceLayer) => {
-        return {
-            ...prev,
-            ...sourceLayer.source,
-        };
-    }, {});
-    const xyzLayers = xyzSourcesLayers.map((sourceLayer) => sourceLayer.layer);
-    return {
-        version: 8,
-        sources: {
-            OSM: {
-                type: 'raster',
-                tiles: [
-                    'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
-                ],
-                tileSize: 256,
-                attribution:
-                    '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-            },
-            ...xyzSources,
-        },
-        layers: [
-            {
-                id: 'OSM',
-                type: 'raster',
-                source: 'OSM',
-                minzoom: 0,
-                maxzoom: 18,
-            },
-            ...xyzLayers,
-        ],
-    };
+const makeMapStyle = (xyzUrls: string[]): StyleSpecification => {
+	const xyzSourcesLayers = xyzUrls.map((xyzUrl) =>
+		makeXyzTileSourceLayer(xyzUrl),
+	);
+	const xyzSources = xyzSourcesLayers.reduce((prev, sourceLayer) => {
+		return {
+			...prev,
+			...sourceLayer.source,
+		};
+	}, {});
+	const xyzLayers = xyzSourcesLayers.map((sourceLayer) => sourceLayer.layer);
+	return {
+		version: 8,
+		sources: {
+			OSM: {
+				type: 'raster',
+				tiles: [
+					'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
+				],
+				tileSize: 256,
+				attribution:
+					'<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+			},
+			...xyzSources,
+		},
+		layers: [
+			{
+				id: 'OSM',
+				type: 'raster',
+				source: 'OSM',
+				minzoom: 0,
+				maxzoom: 18,
+			},
+			...xyzLayers,
+		],
+	};
 };
 
-const makeXyzTileSourceLayer = (xyzUrl) => {
-    const source = {
-        [xyzUrl]: { type: 'raster', tiles: [xyzUrl], minzoom: 4, maxzoom: 10 },
-    };
-    const layer = {
-        id: xyzUrl,
-        type: 'raster',
-        source: xyzUrl,
-        paint: {
-            'raster-opacity': 0.5,
-        },
-    };
-    return { source, layer };
+const makeXyzTileSourceLayer = (
+	xyzUrl: string,
+): {
+	source: { [key: string]: RasterSourceSpecification };
+	layer: RasterLayerSpecification;
+} => {
+	const source: { [key: string]: RasterSourceSpecification } = {
+		[xyzUrl]: {
+			type: 'raster',
+			tiles: [xyzUrl],
+			minzoom: 4,
+			maxzoom: 10,
+			attribution:
+				"<a href='https://www.jma.go.jp/jma/indexe.html' target='_blank'>Japan Meteorological Agency</a> | <a href='https://twitter.com/kanahiro_iguchi' target='_blank'>@kanahiro_iguchi</a>",
+		},
+	};
+	const layer: RasterLayerSpecification = {
+		id: xyzUrl,
+		type: 'raster',
+		source: xyzUrl,
+		paint: {
+			'raster-opacity': 0.5,
+		},
+	};
+	return { source, layer };
 };
 
 const map = new maplibregl.Map({
-    container: 'map',
-    style: {
-        version: 8,
-        sources: {},
-        layers: [],
-    },
-    center: [136.0, 35.0],
-    zoom: 4,
-    minZoom: 4,
-    maxZoom: 12,
-    customAttribution:
-        "<a href='https://www.jma.go.jp/jma/indexe.html' target='_blank'>Japan Meteorological Agency</a> | <a href='https://twitter.com/kanahiro_iguchi' target='_blank'>@kanahiro_iguchi</a>",
+	container: 'map',
+	style: {
+		version: 8,
+		sources: {},
+		layers: [],
+	},
+	center: [136.0, 35.0],
+	hash: true,
+	bearing: -18,
+	maxPitch: 85,
+	pitch: 60,
+	zoom: 2,
+	minZoom: 5,
+	maxZoom: 10,
+});
+map.on('styledata', () => {
+	map.setProjection({ type: 'globe' });
 });
 
 const formatDatetimeText = (yyyymmddHHMMSS) => {
-    // ex) https://www.jma.go.jp/bosai/jmatile/data/nowc/20210813094000/none/20210813094000/surf/hrpns/{z}/{x}/{y}.png
-    const yyyy = yyyymmddHHMMSS.substring(0, 4);
-    const mm = yyyymmddHHMMSS.substring(4, 6);
-    const dd = yyyymmddHHMMSS.substring(6, 8);
-    const HH = yyyymmddHHMMSS.substring(8, 10);
-    const MM = yyyymmddHHMMSS.substring(10, 12);
-    const SS = yyyymmddHHMMSS.substring(12);
-    return `${yyyy}-${mm}-${dd}T${HH}:${MM}:${SS}Z`;
+	// ex) https://www.jma.go.jp/bosai/jmatile/data/nowc/20210813094000/none/20210813094000/surf/hrpns/{z}/{x}/{y}.png
+	const yyyy = yyyymmddHHMMSS.substring(0, 4);
+	const mm = yyyymmddHHMMSS.substring(4, 6);
+	const dd = yyyymmddHHMMSS.substring(6, 8);
+	const HH = yyyymmddHHMMSS.substring(8, 10);
+	const MM = yyyymmddHHMMSS.substring(10, 12);
+	const SS = yyyymmddHHMMSS.substring(12);
+	return `${yyyy}-${mm}-${dd}T${HH}:${MM}:${SS}Z`;
 };
 
 nowcast.getTimeData().then((timedata) => {
-    const tileUrlData = nowcast.getXyzTileUrlData(timedata);
-    const tileUrls = [
-        ...tileUrlData.past,
-        tileUrlData.now,
-        ...tileUrlData.forecast,
-    ];
-    const mapStyle = makeMapStyle(tileUrls);
-    map.setStyle(mapStyle);
+	const tileUrlData = nowcast.getXyzTileUrlData(timedata);
+	const tileUrls = [
+		...tileUrlData.past,
+		tileUrlData.now,
+		...tileUrlData.forecast,
+	];
+	const mapStyle = makeMapStyle(tileUrls);
+	map.setStyle(mapStyle);
 
-    const overlayLayers = mapStyle.layers.slice(1); //layers[0] = background
-    const temporalFrames = overlayLayers.map((layer) => ({
-        layers: [layer],
-        title: formatDatetimeText(layer.id.substring(66, 80)),
-    }));
+	const overlayLayers = mapStyle.layers.slice(1); //layers[0] = background
+	const temporalFrames = overlayLayers.map((layer) => ({
+		layers: [layer],
+		title: formatDatetimeText(layer.id.substring(66, 80)),
+	}));
 
-    const temporalControl = new TemporalControl(temporalFrames, {
-        interval: 100,
-    });
-    map.addControl(temporalControl);
+	const temporalControl = new TemporalControl(temporalFrames, {
+		interval: 100,
+	});
+	map.addControl(temporalControl);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,238 +1,237 @@
-import { AnyLayer, IControl, Map } from 'maplibre-gl';
+import type {
+	IControl,
+	Map,
+	LayerSpecification,
+	ControlPosition,
+} from 'maplibre-gl';
 
 import { play, pause, reload, skipBackward, skipForward } from './icons';
 
 const ACTIVE_BUTTON_COLOR = 'rgb(204, 204, 204)';
 
 type ContainerOptions = {
-    length: number;
-    interval: number;
-    onSliderValueChange: () => void;
+	length: number;
+	interval: number;
+	onSliderValueChange: () => void;
 };
 
 const makeImg = (icon: string): HTMLImageElement => {
-    const img = document.createElement('img');
-    img.src = icon;
-    return img;
+	const img = document.createElement('img');
+	img.src = icon;
+	return img;
 };
 
 const makeContainer = ({
-    length,
-    interval,
-    onSliderValueChange,
+	length,
+	interval,
+	onSliderValueChange,
 }: ContainerOptions): [HTMLDivElement, HTMLDivElement, HTMLInputElement] => {
-    // outest div
-    const container = document.createElement('div');
-    container.classList.add('mapboxgl-ctrl');
-    container.classList.add('mapboxgl-ctrl-group');
-    container.style.width = '240px';
-    container.style.height = '84px';
-    container.style.backgroundColor = '#fff';
-    container.style.textAlign = 'center';
+	// outest div
+	const container = document.createElement('div');
+	container.classList.add('maplibregl-ctrl');
+	container.classList.add('maplibregl-ctrl-group');
+	container.style.width = '240px';
+	container.style.height = '84px';
+	container.style.backgroundColor = '#fff';
+	container.style.textAlign = 'center';
 
-    const titleDiv = document.createElement('div');
-    titleDiv.innerHTML = '<br />';
-    titleDiv.style.marginTop = '4px';
-    container.appendChild(titleDiv);
+	const titleDiv = document.createElement('div');
+	titleDiv.innerHTML = '<br />';
+	titleDiv.style.marginTop = '4px';
+	container.appendChild(titleDiv);
 
-    // temporal slider
-    const slider = document.createElement('input');
-    slider.type = 'range';
-    slider.value = '0';
-    slider.min = '0';
-    slider.max = String(length - 1);
-    slider.addEventListener('input', () => {
-        onSliderValueChange();
-    });
-    slider.style.width = '80%';
-    slider.style.margin = '4px 0';
-    container.appendChild(slider);
+	// temporal slider
+	const slider = document.createElement('input');
+	slider.type = 'range';
+	slider.value = '0';
+	slider.min = '0';
+	slider.max = String(length - 1);
+	slider.addEventListener('input', () => {
+		onSliderValueChange();
+	});
+	slider.style.width = '80%';
+	slider.style.margin = '4px 0';
+	container.appendChild(slider);
 
-    // buttons div
-    // loop, prev, pause, play, next
-    const buttonsDiv = document.createElement('div');
-    buttonsDiv.style.display = 'flex';
-    buttonsDiv.style.justifyContent = 'center';
-    buttonsDiv.style.margin = '4px 0 0 0';
-    const loopButton = document.createElement('button');
-    loopButton.appendChild(makeImg(reload));
-    loopButton.style.border = '0';
-    loopButton.style.borderRadius = '0';
-    loopButton.style.marginRight = '16px';
-    loopButton.style.height = '24px';
-    loopButton.onclick = () => {
-        loopButton.style.backgroundColor =
-            loopButton.style.backgroundColor === '' ? ACTIVE_BUTTON_COLOR : '';
-    };
-    buttonsDiv.appendChild(loopButton);
-    const decrement = () => {
-        slider.value = String(Math.max(0, Number(slider.value) - 1));
-        onSliderValueChange();
-        return Number(slider.min) < Number(slider.value);
-    };
-    const increment = () => {
-        if (
-            loopButton.style.backgroundColor !== '' &&
-            Number(slider.value) == Number(slider.max)
-        ) {
-            while (decrement()) {}
-        } else {
-            slider.value = String(
-                Math.min(Number(slider.max), Number(slider.value) + 1),
-            );
-        }
-        onSliderValueChange();
-        return Number(slider.value) < Number(slider.max);
-    };
-    const prevButton = document.createElement('button');
-    prevButton.appendChild(makeImg(skipBackward));
-    prevButton.onclick = decrement;
-    prevButton.style.border = '0';
-    prevButton.style.height = '24px';
-    const pauseButton = document.createElement('button');
-    pauseButton.appendChild(makeImg(pause));
-    pauseButton.style.border = '0';
-    pauseButton.style.height = '24px';
-    const playButton = document.createElement('button');
-    playButton.appendChild(makeImg(play));
-    playButton.style.border = '0';
-    playButton.style.height = '24px';
-    playButton.onclick = () => {
-        if (playButton.style.backgroundColor === ACTIVE_BUTTON_COLOR) return;
+	// buttons div
+	// loop, prev, pause, play, next
+	const buttonsDiv = document.createElement('div');
+	buttonsDiv.style.display = 'flex';
+	buttonsDiv.style.justifyContent = 'center';
+	buttonsDiv.style.margin = '4px 0 0 0';
+	const loopButton = document.createElement('button');
+	loopButton.appendChild(makeImg(reload));
+	loopButton.style.border = '0';
+	loopButton.style.borderRadius = '0';
+	loopButton.style.marginRight = '16px';
+	loopButton.style.height = '24px';
+	loopButton.onclick = () => {
+		loopButton.style.backgroundColor =
+			loopButton.style.backgroundColor === '' ? ACTIVE_BUTTON_COLOR : '';
+	};
+	buttonsDiv.appendChild(loopButton);
+	const decrement = () => {
+		slider.value = String(Math.max(0, Number(slider.value) - 1));
+		onSliderValueChange();
+		return Number(slider.min) < Number(slider.value);
+	};
+	const increment = () => {
+		if (
+			loopButton.style.backgroundColor !== '' &&
+			Number(slider.value) == Number(slider.max)
+		) {
+			while (decrement()) {}
+		} else {
+			slider.value = String(
+				Math.min(Number(slider.max), Number(slider.value) + 1),
+			);
+		}
+		onSliderValueChange();
+		return Number(slider.value) < Number(slider.max);
+	};
+	const prevButton = document.createElement('button');
+	prevButton.appendChild(makeImg(skipBackward));
+	prevButton.onclick = decrement;
+	prevButton.style.border = '0';
+	prevButton.style.height = '24px';
+	const pauseButton = document.createElement('button');
+	pauseButton.appendChild(makeImg(pause));
+	pauseButton.style.border = '0';
+	pauseButton.style.height = '24px';
+	const playButton = document.createElement('button');
+	playButton.appendChild(makeImg(play));
+	playButton.style.border = '0';
+	playButton.style.height = '24px';
+	playButton.onclick = () => {
+		if (playButton.style.backgroundColor === ACTIVE_BUTTON_COLOR) return;
 
-        playButton.style.backgroundColor = ACTIVE_BUTTON_COLOR;
-        const timerId = setInterval(() => {
-            increment();
-        }, interval);
-        pauseButton.onclick = () => {
-            clearInterval(timerId);
-            pauseButton.onclick = null;
-            playButton.style.backgroundColor = '';
-        };
-    };
-    const nextButton = document.createElement('button');
-    nextButton.appendChild(makeImg(skipForward));
-    nextButton.style.border = '0';
-    nextButton.style.height = '24px';
-    nextButton.onclick = increment;
-    buttonsDiv.appendChild(prevButton);
-    buttonsDiv.appendChild(pauseButton);
-    buttonsDiv.appendChild(playButton);
-    buttonsDiv.appendChild(nextButton);
+		playButton.style.backgroundColor = ACTIVE_BUTTON_COLOR;
+		const timerId = setInterval(() => {
+			increment();
+		}, interval);
+		pauseButton.onclick = () => {
+			clearInterval(timerId);
+			pauseButton.onclick = null;
+			playButton.style.backgroundColor = '';
+		};
+	};
+	const nextButton = document.createElement('button');
+	nextButton.appendChild(makeImg(skipForward));
+	nextButton.style.border = '0';
+	nextButton.style.height = '24px';
+	nextButton.onclick = increment;
+	buttonsDiv.appendChild(prevButton);
+	buttonsDiv.appendChild(pauseButton);
+	buttonsDiv.appendChild(playButton);
+	buttonsDiv.appendChild(nextButton);
 
-    container.appendChild(buttonsDiv);
+	container.appendChild(buttonsDiv);
 
-    return [container, titleDiv, slider];
+	return [container, titleDiv, slider];
 };
 
 type Position = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
 
 type TemporalFrame = {
-    title: string;
-    layers: AnyLayer[];
+	title: string;
+	layers: LayerSpecification[];
 };
 
 type Options = {
-    position?: Position;
-    interval?: number;
-    performance?: boolean;
+	position?: Position;
+	interval?: number;
+	performance?: boolean;
 };
 
 export default class TemporalControl implements IControl {
-    private map: Map | undefined;
-    private options: Options;
+	private map: Map | undefined;
+	private options: Options;
 
-    private container: HTMLDivElement;
-    private containerTitle!: HTMLDivElement;
-    private temporalSlider!: HTMLInputElement;
-    private temporalFrames: TemporalFrame[];
+	private container: HTMLDivElement;
+	private containerTitle!: HTMLDivElement;
+	private temporalSlider!: HTMLInputElement;
+	private temporalFrames: TemporalFrame[];
 
-    constructor(temporalFrames: TemporalFrame[], options: Options = {}) {
-        this.temporalFrames = temporalFrames;
-        this.options = options;
+	constructor(temporalFrames: TemporalFrame[], options: Options = {}) {
+		this.temporalFrames = temporalFrames;
+		this.options = options;
 
-        const containerOptions: ContainerOptions = {
-            length: this.temporalFrames.length,
-            interval: this.options.interval || 500,
-            onSliderValueChange: () => this.refresh(),
-        };
+		const containerOptions: ContainerOptions = {
+			length: this.temporalFrames.length,
+			interval: this.options.interval || 500,
+			onSliderValueChange: () => this.refresh(),
+		};
 
-        [this.container, this.containerTitle, this.temporalSlider] =
-            makeContainer(containerOptions);
-    }
+		[this.container, this.containerTitle, this.temporalSlider] =
+			makeContainer(containerOptions);
+	}
 
-    onAdd(map: Map) {
-        this.map = map;
-        map.getContainer().appendChild(this.container);
+	onAdd(map: Map) {
+		this.map = map;
+		map.getContainer().appendChild(this.container);
 
-        this.map.once('styledata', () => {
-            this.refresh();
-        });
+		this.map.once('styledata', () => {
+			this.refresh();
+		});
 
-        return this.container;
-    }
+		return this.container;
+	}
 
-    onRemove() {
-        this.container.parentNode?.removeChild(this.container);
-        this.map = undefined;
-    }
+	onRemove() {
+		this.container.parentNode?.removeChild(this.container);
+		this.map = undefined;
+	}
 
-    getDefaultPosition(): string {
-        return 'bottom-left';
-    }
+	getDefaultPosition(): ControlPosition {
+		return 'bottom-left';
+	}
 
-    refresh() {
-        const sliderValue = Number(this.temporalSlider.value);
-        this.containerTitle.innerHTML = this.temporalFrames[sliderValue].title;
-        const visibleLayerIds = this.temporalFrames[sliderValue].layers.map(
-            (layer) => layer.id,
-        );
-        this.temporalFrames.forEach((temporalFrame) => {
-            temporalFrame.layers.forEach((layer) =>
-                this.setVisible(layer, visibleLayerIds.includes(layer.id)),
-            );
-        });
-    }
+	refresh() {
+		const sliderValue = Number(this.temporalSlider.value);
+		this.containerTitle.innerHTML = this.temporalFrames[sliderValue].title;
+		const visibleLayerIds = this.temporalFrames[sliderValue].layers.map(
+			(layer) => layer.id,
+		);
+		this.temporalFrames.forEach((temporalFrame) => {
+			temporalFrame.layers.forEach((layer) =>
+				this.setVisible(layer, visibleLayerIds.includes(layer.id)),
+			);
+		});
+	}
 
-    private setVisible(layer: AnyLayer, isVisible = true) {
-        if (
-            layer.type === 'raster' ||
-            layer.type === 'fill' ||
-            layer.type === 'circle' ||
-            layer.type === 'line'
-        ) {
-            if (layer.type === 'raster') {
-                // when raster, set opacity as visibility for background loading
-                this.map?.setPaintProperty(
-                    layer.id,
-                    `${layer.type}-opacity-transition`,
-                    {
-                        // set disable fade-in transition
-                        duration: 0,
-                    },
-                );
-            }
-            let opacity;
-            if (isVisible) {
-                // @ts-ignore
-                opacity = layer.paint?.[`${layer.type}-opacity`] || 1;
-            } else {
-                opacity = this.options.performance
-                    ? 0.000000000000000000001
-                    : 0;
-            }
+	private setVisible(layer: LayerSpecification, isVisible = true) {
+		if (
+			layer.type === 'raster' ||
+			layer.type === 'fill' ||
+			layer.type === 'circle' ||
+			layer.type === 'line'
+		) {
+			if (layer.type === 'raster') {
+				// when raster, set opacity as visibility for background loading
+				this.map?.setPaintProperty(
+					layer.id,
+					`${layer.type}-opacity-transition`,
+					{
+						// set disable fade-in transition
+						duration: 0,
+					},
+				);
+			}
+			let opacity;
+			if (isVisible) {
+				// @ts-ignore
+				opacity = layer.paint?.[`${layer.type}-opacity`] || 1;
+			} else {
+				opacity = this.options.performance ? 0.000000000000000000001 : 0;
+			}
 
-            this.map?.setPaintProperty(
-                layer.id,
-                `${layer.type}-opacity`,
-                opacity,
-            );
-        } else {
-            this.map?.setLayoutProperty(
-                layer.id,
-                'visibility',
-                isVisible ? 'visible' : 'none',
-            );
-        }
-    }
+			this.map?.setPaintProperty(layer.id, `${layer.type}-opacity`, opacity);
+		} else {
+			this.map?.setLayoutProperty(
+				layer.id,
+				'visibility',
+				isVisible ? 'visible' : 'none',
+			);
+		}
+	}
 }

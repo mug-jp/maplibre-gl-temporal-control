b0d32b7da2cea12292fbb581c120b54464c0a459
zhoujinze <zhoujinze@gmail.com>
2024-12-30 17:12:27 +1300

    Removing build warnings and adding an example index page

--- vite.config.example.ts
+++ vite.config.example.ts
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	root: './example',
 	base: './',
 	build: {
 		outDir: '../dist',
+    chunkSizeWarningLimit: 1000,
+    emptyOutDir: true,
 		rollupOptions: {
 			input: {
+        index: 'example/index.html',
 				raster: 'example/raster.html',
 				vector: 'example/vector.html',
 			},
 		},
 	},
+  plugins: [
+    visualizer({
+      filename: './dist/stats.html',
+      open: false,
+    }),
+  ],
 });
+
